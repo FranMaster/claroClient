4f20a1eea4246a0718268d2bb0f886088c70612d
Francisco <Francisco.jimenez.cohen@gmail.com>
2020-06-06 01:42:46 -0300

    Realizacion de Login

--- ClaroNet/ClaroNet/App.xaml.cs
+++ ClaroNet/ClaroNet/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClaroNet.views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace ClaroNet
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
